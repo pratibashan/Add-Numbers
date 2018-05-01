9f90d1bda555945d0729e6d82aeebc1d6950031a
Pratiba Shan <pratibashan@Pratibas-MBP.localdomain>
2018-05-01 13:21:53 -0500

    Adding two numbers

---
+++ add_numbers.py
@@ -0,0 +1,9 @@
+
+#Adding two numbers
+
+number1 = float(input("Enter the first number: "))
+
+number2 = float(input("Enter the second number: "))
+
+
+print (f"The first number is {number1} , the second number is {number2} and the total is  : {number1 + number2}")
